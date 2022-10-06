f052277d93a0a4699ded3411a5ed18533cc495d9
vijayshiv <vijay.aryaa1213@gmail.com>
2022-10-06 17:20:31 +0530

    Edited

---
+++ src/com/StringsandStringBuilder/NewPattern.java
@@ -0,0 +1,23 @@
+package com.StringsandStringBuilder;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class NewPattern {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the length : ");
+        int n = in.nextInt();
+
+        int originalN = n;
+        n = (2 * n) - 2;
+
+        for (int row = 1; row < n; row++) {
+            for (int col = 1; col < n ; col++) {
+                int atEveryIndex = originalN - Math.min(Math.min(row, col), Math.min(n - row, n - col));
+                System.out.print(atEveryIndex + " ");
+            }
+            System.out.println();
+        }
+    }
+}
