c83967e53b75caf1789a2c4e911191ccde0162d9
vijayshiv <vijay.aryaa1213@gmail.com>
2023-05-23 02:32:05 +0530

    Edited

---
+++ src/com/Recursion/rough.java
@@ -0,0 +1,17 @@
+package com.Recursion;
+
+import java.util.Scanner;
+
+public class rough {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int n = in.nextInt();
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n ; j++) {
+
+            }
+        }
+    }
+}
